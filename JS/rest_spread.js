0ec285d8a9a50ca04c9f430ff9e05393e489b66f
pamuba <partha.bora24@gmail.com>
2021-08-10 00:36:44 +0530

    72

---
+++ JS/rest_spread.js
@@ -0,0 +1,37 @@
+// function sum(a,b,...args){
+//     let sum = 0;
+//     for(let i=0; i<args.length ; i++){
+//         sum += args[i]
+//     }
+//     console.log("SUM:", sum)
+//     console.log(args instanceof Array)
+// }
+// sum(1,2,3,4)
+
+
+//argumenst
+// function sum(){
+//     let sum = 0;
+//     for(let i=0; i<arguments.length ; i++){
+//         sum += arguments[i]
+//     }
+//     console.log("SUM:", sum)
+//     console.log(arguments instanceof Array)
+// }
+// sum(1,2,3,4)
+
+//spread
+// console.log(Math.max(23,4,9))
+
+// let arr = [3,1,5]
+// let ar = [1212,54,6565]
+// console.log(Math.max(...arr, ...ar, 102))
+
+// let a = 45;
+// let b = 99;
+
+// [a,b] = [b,a]
+// console.log(`a:${a} b:${b}`)
+
+let arr = [12,3,3,4,4,5,6,7,12]
+
---
+++ JS/var_let.js
@@ -0,0 +1,54 @@
+// var , let 
+
+//Scoping 
+// function run(){
+//     var foo = "Foo";
+//     var bar = "Bar";
+
+//     console.log(foo, bar)
+//     {
+//         var moo ="Moo"
+//         let bazz = "Bazz"
+//         console.log(moo, bazz)
+//     }
+//     // console.log(moo)
+//     // console.log(bazz)
+
+// }
+// run();
+
+// var funcs = []
+// for(let i=0; i<3;i++){
+//     funcs[i] = function(){
+//         console.log("My Value:"+i)
+//     }
+// }
+
+// for(var j=0; j<3;j++){
+//     funcs[j]();
+// }
+
+//hoisting
+// function  run(){
+//     // var foo;
+
+//     console.log(foo)
+//     let foo = "Foo";
+// }
+// run()
+
+
+//redecleration
+// let a = 13;
+// let a = 100;
+// console.log(a)
+
+
+
+
+//var / no var
+function run(){
+    a = 24
+}
+run()
+console.log(a)
