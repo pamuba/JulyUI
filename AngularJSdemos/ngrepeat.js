dad23cc2bddfdd00f6a5f816184fd20f55d482cd
pamuba <partha.bora24@gmail.com>
2021-08-20 00:45:38 +0530

    117

--- AngularJSdemos/ngrepeat.js
+++ AngularJSdemos/ngrepeat.js
@@ -10,6 +10,34 @@ var myApp = angular
                         { firstName: "Todd", lastName: "Barber", gender: "Male", salary: 60000 }
                     ];
 
+                    var countries = [
+                        {
+                            name: "UK",
+                            cities: [
+                                { name: "London" },
+                                { name: "Birmingham" },
+                                { name: "Manchester" }
+                            ]
+                        },
+                        {
+                            name: "USA",
+                            cities: [
+                                { name: "Los Angeles" },
+                                { name: "Chicago" },
+                                { name: "Houston" }
+                            ]
+                        },
+                        {
+                            name: "India",
+                            cities: [
+                                { name: "Hyderabad" },
+                                { name: "Chennai" },
+                                { name: "Mumbai" }
+                            ]
+                        }
+                    ];
+
                     $scope.employees = employees;
+                    $scope.countries = countries;
                 })
 
