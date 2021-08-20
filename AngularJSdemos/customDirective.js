5d346acc5ed2613c94f6ff6d87b80d4708ce8c0d
pamuba <partha.bora24@gmail.com>
2021-08-21 00:16:33 +0530

    122

--- AngularJSdemos/customDirective.js
+++ AngularJSdemos/customDirective.js
@@ -2,6 +2,18 @@
 var myApp = angular
                 .module("myModule",[])
                 .controller("myController",function($scope){
-                  
+                  $scope.msg = "Message from Controller"
                 })
+                .controller("myController1",function($scope){
+                    $scope.msg = "Message from Controller1"
+                });
+
+//Custom Directive
+myApp.directive('myInfoMsg', function(){
+    return {
+        // template:"<h2>This is a message</h2>"
+        // template:"<h2>{{msg}}</h2>"
+        templateUrl : "my-info-msg.html"
+    }
+})
 
