6d5d1d4fc07248b9aa786c3fd7d8185c455079bb
pamuba <partha.bora24@gmail.com>
2021-08-24 00:47:37 +0530

    1344

---
+++ AngularJSdemos/digest.js
@@ -0,0 +1,24 @@
+//1. Create a module
+var myApp = angular
+  .module("myModule", [])
+  .controller("myController", function ($scope, $rootScope) {
+     $scope.a = 1;
+     $scope.b = 2;
+     $scope.s = 0;
+    
+     $scope.calcSum = function(){
+         $scope.s = Number($scope.a) + Number($scope.b);
+     }
+  });
+
+
+  var btnClick = function(){
+      var $scope = angular.element($("#div1")).scope();
+
+    //   $scope.s =  Number($scope.a) + Number($scope.b);
+    //   $scope.$apply();
+
+    $scope.$apply(function(){
+        $scope.s =  Number($scope.a) + Number($scope.b);
+    })
+  }
