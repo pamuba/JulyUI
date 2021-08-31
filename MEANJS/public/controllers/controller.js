71b859864dbe822c9096fd38066c5a86611605cb
pamuba <partha.bora24@gmail.com>
2021-09-01 01:14:57 +0530

    1923

--- MEANJS/public/controllers/controller.js
+++ MEANJS/public/controllers/controller.js
@@ -1,7 +1,41 @@
 function AppCtrl($scope, $http){
     console.log("Hello from Controller")
 
-    $http.get('/contactlist').success(function(response){
-        $scope.contactlist = response  
-    })
+    var refresh = function(){
+        $http.get('/contactlist').success(function(response){
+            $scope.contactlist = response 
+            $scope.contact = '' 
+        });
+    }
+
+    refresh();    
+    
+
+    $scope.addContact = function(){
+        console.log($scope.contact)
+        $http.post('/contactlist', $scope.contact).success(function(response){
+            refresh();
+        })
+    }
+
+    $scope.remove = function(id){
+        console.log(id)
+        $http.delete('/contactlist/'+id).success(function(response){
+            refresh();
+        })
+    }
+
+    $scope.edit = function(id){
+        console.log(id)
+        $http.get('/contactlist/'+id).success(function(response){
+            $scope.contact = response
+        })
+    }
+
+    $scope.update = function(){
+        console.log($scope.contact._id);
+        $http.put('/contactlist/'+$scope.contact._id, $scope.contact).success(function(response){
+            refresh();
+        })
+    }
 }
--- MEANJS/server.js
+++ MEANJS/server.js
@@ -5,7 +5,10 @@ let mongojs = require('mongojs')
 let db = mongojs('contactlist', ['contactlist'])
 
 let app = express()
+app.use(express.json())
 
+//tell express about static folder
+app.use(express.static(__dirname+'/public'))
 
 app.get("/contactlist", function(req, res){
     console.log('Express got a request from the Controller')
@@ -17,8 +20,45 @@ app.get("/contactlist", function(req, res){
     })
 })
 
-//tell express about static folder
-app.use(express.static(__dirname+'/public'))
+app.post("/contactlist", function(req, res){
+    console.log(req.body)
+    db.contactlist.insert(req.body, function(err, doc){
+        if(err === null){
+            res.json(doc)
+        } 
+    })
+})
+
+app.delete("/contactlist/:id", function(req, res){
+    var id = req.params.id;
+    console.log(id)
+    db.contactlist.remove({_id:mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc)
+    })
+})
+
+app.get("/contactlist/:id", function(req, res){
+    var id = req.params.id;
+    console.log(id)
+    db.contactlist.findOne({_id:mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc)
+    })
+})
+
+
+app.put("/contactlist/:id", function(req, res){
+    var id = req.params.id;
+    console.log(req.params.body)
+
+    db.contactlist.findAndModify({query:{_id:mongojs.ObjectId(id)}, 
+        update: {$set:{name:req.body.name, email:req.body.email, number:req.body.number}},
+        new:true}, function(err, doc){
+            res.json(doc);
+    })
+})
+
+
+
 
 app.listen("3000", "127.0.0.1", function(){
     console.log("Server listening @3000")
