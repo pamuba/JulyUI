e45c382e4ebd9756ea345614aaa2f0182feb3858
pamuba <partha.bora24@gmail.com>
2021-09-15 20:39:07 +0530

    2633

--- Angular9/angularauth/server/routes/api.js
+++ Angular9/angularauth/server/routes/api.js
@@ -1,9 +1,154 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const db = "mongodb://localhost:27017/authdb"
+
+mongoose.connect(db, err=>{
+    if(err){
+        console.log('Error!'+err)
+    }
+    else{
+        console.log('Connected to MongoDB')
+    }
+})
 
 // localhost:3000/api === /
 router.get('/', (req, res)=>{
     res.send('From Api route')
 })
+
+
+//REGISTER
+router.post('/register', (req, res)=>{
+    let userData = req.body
+    let user = new User(userData)
+    user.save((error, registeredUser)=>{
+        if(error){
+            console.log(error)
+        }
+        else{
+            res.status(200).send(registeredUser)
+        }
+    })
+})
+
+//Login API
+router.post('/login', (req,res) => {
+    let userData = req.body
+
+    //Mongoose Function
+    User.findOne({email:userData.email}, (error, user)=>{
+        if(error){
+            console.log(error);
+        }
+        else
+        {
+            //case 1
+            if(!user){
+                res.status(401).send("Invalid email");
+            }else{
+                if(user.password !== userData.password){
+                    res.status(401).send("Invalid password");
+                }
+                else{
+                    let payload = {subject:user._id}
+                    let token = jwt.sign(payload, 'secretKey')
+                    res.status(200).send({token})
+                    // res.status(200).send(user)
+                }
+            }
+        }
+
+    })
+})
+
+router.get('/events', (req, res)=>{
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+          },
+          {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+          },
+          {
+            "_id": "3",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+          },
+          {
+            "_id": "4",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+          },
+          {
+            "_id": "5",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+          },
+          {
+            "_id": "6",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+          }
+    ]
+    res.json(events)
+})
+
+
+router.get('/special', (req, res)=>{
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+          },
+          {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+          },
+          {
+            "_id": "3",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+          },
+          {
+            "_id": "4",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+          },
+          {
+            "_id": "5",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+          },
+          {
+            "_id": "6",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+          }
+    ]
+    res.json(events)
+})
+
 
 module.exports = router
