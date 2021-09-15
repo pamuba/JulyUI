7aac1db256ed72c3bfe2afc3a1595307bfd389be
pamuba <partha.bora24@gmail.com>
2021-09-15 21:27:09 +0530

    26333

--- Angular9/angularauth/ngApp/src/app/app.module.ts
+++ Angular9/angularauth/ngApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
@@ -18,7 +19,9 @@ import { SpecialEventsComponent } from './special-events/special-events.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular9/angularauth/ngApp/src/app/auth.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private _registerUrl = "http://localhost:3000/api/register"
+  constructor(private http:HttpClient) { }
+
+  registerUser(user){
+    return this.http.post(this._registerUrl,user)
+  }
+}
