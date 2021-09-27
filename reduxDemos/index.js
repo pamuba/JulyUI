fd109d8613f54ada21d5071141d73b865e1c633f
pamuba <partha.bora24@gmail.com>
2021-09-27 20:32:25 +0530

    332

--- reduxDemos/index.js
+++ reduxDemos/index.js
@@ -1,5 +1,6 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 //action is an object with type property
 const BUY_CAKE = 'BUY_CAKE'
@@ -18,17 +19,38 @@ function buyIceCream(){
 
 //reducer is a fn (previousState, action)
 
-const initailState = {
+// const initailState = {
+//     numOfCakes:10,
+//     numOfIceCreams:20
+// }
+
+const initialCakeState = {
     numOfCakes:10,
+}
+
+const initialIceCreamState = {
     numOfIceCreams:20
 }
 
-const reducer = (state = initailState, action) => {
+// const reducer = (state = initailState, action) => {
+//     switch(action.type){
+//         case BUY_CAKE: return {
+//             ...state,
+//             numOfCakes: state.numOfCakes - 1
+//         }
+//         case BUY_ICECREAM: return {
+//             ...state,
+//             numOfIceCreams: state.numOfIceCreams - 1
+//         }
+//         default: return state
+//     }
+// }
+
+
+
+
+const iceCreaRreducer = (state = initialIceCreamState, action) => {
     switch(action.type){
-        case BUY_CAKE: return {
-            ...state,
-            numOfCakes: state.numOfCakes - 1
-        }
         case BUY_ICECREAM: return {
             ...state,
             numOfIceCreams: state.numOfIceCreams - 1
@@ -37,9 +59,23 @@ const reducer = (state = initailState, action) => {
     }
 }
 
+const cakeReducer = (state = initialCakeState, action) => {
+    switch(action.type){
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        default: return state
+    }
+}
+
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreaRreducer
+})
 
 //create the store
-const store = createStore(reducer)
+const store = createStore(rootReducer)
 console.log('Initial State', store.getState())
 const unsubscribe = store.subscribe(()=> console.log('Updated state', store.getState()))
 store.dispatch(buyCake())
