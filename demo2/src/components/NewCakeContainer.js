b9135992f0f4731e8291d81c5c2e13b12c4754a1
pamuba <partha.bora24@gmail.com>
2021-09-28 19:55:46 +0530

    2111

---
+++ demo2/src/components/NewCakeContainer.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react'
+import { buyCake } from '../redux'
+import { connect } from 'react-redux'
+
+function NewCakeContainer(props) {
+    const [number, setNumber] = useState()
+    return (
+        <>
+            <h1>Num of Cakes - {props.numOfCakes}</h1>
+            <input type="text" value={number} onChange={e => setNumber(e.target.value)} />
+            <button onClick={() => props.buyCake(number)}>Buy {number} Cakes</button>
+        </>
+    )
+}
+
+
+const mapStateToProps = state => {
+    return {
+        numOfCakes : state.cake.numOfCakes
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewCakeContainer)
--- demo2/src/redux/store.js
+++ demo2/src/redux/store.js
@@ -1,7 +1,8 @@
-import {createStore} from 'redux'
-// import cakeReducer from './cakes/cakeReducer'
+import {createStore, applyMiddleware} from 'redux'
 import rootReducer from './rootReducer';
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
 
 export default store;
