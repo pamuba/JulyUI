f6c2da4dacdefbb7ff683435e29eee94d00aba52
pamuba <partha.bora24@gmail.com>
2021-09-28 19:34:08 +0530

    341

--- demo2/src/App.js
+++ demo2/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer'
+import IceCreamContainer from './components/IceCreamContainer';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <div className="App">
         <header className="App-header">
           <CakeContainer />
+          <HooksCakeContainer/>
+          <IceCreamContainer/>
         </header>
       </div>
     </Provider>
---
+++ demo2/src/components/IceCreamContainer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { buyIceCream } from '../redux'
+import { connect } from 'react-redux'
+
+function IceCreamContainer(props) {
+    return (
+        <>
+            <h1>Num of IceCreams - {props.numOfIceCreams}</h1>
+            <button onClick={props.buyIceCream}>Buy IceCream</button>
+        </>
+    )
+}
+
+
+const mapStateToProps = state => {
+    return {
+        numOfIceCreams : state.iceCream.numOfIceCreams
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        buyIceCream: () => dispatch(buyIceCream())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(IceCreamContainer)
