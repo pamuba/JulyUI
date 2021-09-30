e095281fe3a9c9fbdc751c733382e399e9f08654
pamuba <partha.bora24@gmail.com>
2021-09-30 19:00:25 +0530

    381

---
+++ reduxsaga/src/actions/users.js
@@ -0,0 +1,16 @@
+export const Types = {
+    GET_USERS_REQUEST: 'users/get_users_request',
+    GET_USERS_SUCCESS: 'users/get_users_success'
+}
+
+export const getUsersRequest = () => ({
+    type: Types.GET_USERS_REQUEST
+})
+
+export const getUsersSuccess = ({items}) => ({
+    types:Types.GET_USERS_SUCCESS,
+    payload:{
+        items
+    }
+})
+
